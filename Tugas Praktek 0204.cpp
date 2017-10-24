adf253018a3a91bc8cacdb26495c668d8f5f27c4
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-10-24 18:43:53 +0800

    Input Luas Rumah

--- Tugas Praktek 0204.cpp
+++ Tugas Praktek 0204.cpp
@@ -7,5 +7,8 @@ int main (){
 	double Luas_1box=Lantai_parket*8/1000000;
 	double Jml_box_parket, Biaya_parket,Biaya_pasang,Totalbyr;
 		
+	cout<<"Luas Rumah :"<<Luas_rumah<<"m2"<<endl;
+	cout<<"Luas 1 Box Parket:"<<Jml_box_parket<<endl;
+	
 
 }
