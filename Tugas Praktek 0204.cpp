edf117d8eb209380377cd7a663c9f1d9e8d5ac53
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-10-24 18:37:10 +0800

    Inisialisasi L.Rumah, Lantai Parket,Luas 1box,,Jumlah box parket

---
+++ Tugas Praktek 0204.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	double Luas_Rumah=42;
+	double Lantai_parket=193*1215;
+	double Luas_1box=Lantai_parket*8/1000000;
+	double Jml_box_parket, Biaya_parket,Biaya_pasang,Totalbyr;
+		
+
+}
