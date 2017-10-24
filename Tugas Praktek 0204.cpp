764b96a17bf2785b5dde58a3e97d8844dccadfe2
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-10-24 18:48:07 +0800

    Menghitung Biaya Pasang

--- Tugas Praktek 0204.cpp
+++ Tugas Praktek 0204.cpp
@@ -16,4 +16,7 @@ int main (){
 
 	Biaya_parket=Jml_box_parket*500;
 	cout<<"Biaya Parket (Rp):"<<Biaya_parket<<endl;
+
+	Biaya_pasang=Luas_Rumah*20;
+	cout<<"Biaya Pasang (Rp):"<<Biaya_pasang<<endl;
 }
